ae181940b5d2d8f60eeb2d11ce9a019216855714
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-02-11 00:15:30 +0530

    Angular5 - Two way Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,7 +13,7 @@ export class ServersComponent implements OnInit{
 
   allowNewServer = false;
   serverCreationStatus = 'No Server was created!';
-  serverName = '';
+  serverName = 'TestServer';
 
   constructor() {
     setTimeout(() => {
