e6bf264c28d9237d0095715681c52d55b3fdf87d
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-02-11 20:40:08 +0530

    Angular5 - Directives Assignment - Solution -  Getting the Index when using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ export class AppComponent {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
 
