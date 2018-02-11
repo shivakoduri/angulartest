03d8854ffd1a9dda09e432bf1a3dd823efab7cee
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-02-11 20:23:43 +0530

    Angular5 - Directives Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  showSecret = false;
-  log = [];
-
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
-  }
 }
+
