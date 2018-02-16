d320629e51d105b540796ac1a3d1a45b27aaac3c
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-02-16 09:33:10 +0530

    Example App  - Adding components inside other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from "@angular/forms";
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
@@ -24,7 +25,8 @@ import {ShoppingEditComponent} from "./shopping-list/shopping-edit/shopping-edit
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
