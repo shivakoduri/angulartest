86f76e9107477f1094314c99c31d01243eebf473
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-02-19 03:59:05 +0530

    Angular5 : Components & Data Binding - Binding to Custom Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{type: 'server', name: 'TestServer', content: 'Just a Test!'}];
 
+  onServerAdded(serverData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(bluePrintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
 
 
 }
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from "@angular/core";
+///<reference path="../../../node_modules/@angular/core/src/event_emitter.d.ts"/>
+import {Component, EventEmitter, OnInit, Output} from "@angular/core";
 
 @Component({
   selector: 'app-cockpit',
@@ -6,6 +7,9 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();;
+
   newServerName = '';
   newServerContent = '';
 
@@ -15,18 +19,16 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
   onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.bluePrintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 }
